c9dc9bf7597c15ba0368216b15c7c9a62f0b2c7e
Jot10 <111444746+Jot10@users.noreply.github.com>
2024-07-19 21:12:10 +0530

    Add files via upload

---
+++ const/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const Color blueapp=Color(0xFF4157FF);
+const Color greycolor=Colors.grey;
+const Color greycont=Color.fromARGB(255, 245, 245, 246);
+const Color appbackground= Color(0xFFF7FBFF);
+const Color whitecolor=Colors.white;
+const Color textcolor=Color(0xFF090F47);
+const Color cartcolor=Color(0xFF091C3F73);
+const Color profilecolor=Color.fromRGBO(14, 81, 255, 0.8);
+const Color profiletext=Color.fromRGBO(9, 28, 63, 0.45);
+const Color increasebuttoncolor= Color(0xFFA0ABFF);
+const Color decreasebuttoncolor= Color(0xFFDFE3FF);
---
+++ const/style.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:medical_store_mobile_app/const/colors.dart';
+
+const TextStyle textstylew700=TextStyle(fontWeight: FontWeight.w700,);
+const TextStyle textstylew300=TextStyle(fontWeight: FontWeight.w300,);
+const TextStyle textstylew500=TextStyle(fontWeight: FontWeight.w500,);
+const TextStyle greytext=TextStyle(color: Colors.grey);
+const TextStyle whitetext=TextStyle(color: Colors.white);
+const TextStyle bluetext=TextStyle(color: blueapp);
